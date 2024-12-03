ce1e9865b53c3166fb5d0ddb541e550d916f76c2
kibbae <89984567+kibbae@users.noreply.github.com>
2024-12-03 12:06:55 +0000

    merge bras

--- UI/find_class.py
+++ UI/find_class.py
@@ -5,7 +5,7 @@ from Logic.find_classes import Classes
 def get_class_by_id(file_path, target_id,date):
     classId = Classes(file_path, target_id, date).get_current_capacity_of_class()
     if classId == None:
-        print(f"no class this day with {target_id} id or {target_id} invalid")
+        print(f"no class this day with {target_id} id or {target_id} invalid!")
     else:
         print(f" {classId['date']} {classId['class']}: {classId['currentCapacity']}/{classId['maxCapacity']}")
 
