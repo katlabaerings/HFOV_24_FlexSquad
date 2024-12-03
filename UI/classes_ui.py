041b82b5fc0daaa797168c1d254be41e65d851fe
katlabaerings <katla21@ru.is>
2024-12-03 12:54:45 +0000

    Update classes_ui.py

--- UI/classes_ui.py
+++ UI/classes_ui.py
@@ -1,6 +1,14 @@
+import os
+import sys
+
+# Dynamically add the parent directory to sys.path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(script_dir, '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+    
 from Data.read_data import Data
 from Logic.class_logic import get_all_classes
-
 
 def display_all_classes():
     all_classes = get_all_classes()
