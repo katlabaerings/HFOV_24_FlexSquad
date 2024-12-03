2de0eee72e246f99a08ca4c8b69658ea1facd2d3
KatrinTinna <katrintinnas@gmail.com>
2024-12-03 12:53:06 +0000

    Display all classes modified.

--- UI/classes_ui.py
+++ UI/classes_ui.py
@@ -6,5 +6,13 @@ def display_all_classes():
     all_classes = get_all_classes()
     for a_class in all_classes:
         print(
-            f"{a_class.name} {a_class.date} At: {a_class.time} Teached by: {a_class.trainer.name}"
+            f"""{a_class.class_name} 
+                {a_class.date} At: {a_class.time} 
+                Teached by: {a_class.trainer.firstname} + {a_class.trainer.lastname}
+"""
         )
+
+
+if __name__ == "__main__":
+    display_all_classes()
+
