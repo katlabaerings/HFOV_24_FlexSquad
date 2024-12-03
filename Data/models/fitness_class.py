2af69621ff2adbbcde57759ffa3b123521c9148a
Tomas Palsson <tomas2212042710@gmail.com>
2024-12-03 12:14:11 +0000

    add model classes

---
+++ Data/models/fitness_class.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FitnessClass:
+    id: int
+    class_name: str
+    max_capacity: int
+    current_capacity: int
+    members: str
+    trainer_id: int
+    time: str
+    date: str
--- Data/read_data.py
+++ Data/read_data.py
@@ -1,18 +1,22 @@
 import csv
 import os
 
+from Data.models.member import Member
+from Data.models.fitness_class import FitnessClass
+
+
 class Data:
-    CLASS_FILE_PATH = "Data/class_data.csv"
-    MANAGER_FILE_PATH = "Data/manager_data.csv"
-    MEMBER_FILE_PATH = "Data/member_data.csv"
-    SUBSCRIPTION_FILE_PATH = "Data/subscription_data.csv"
+    CLASS_FILE_PATH = "../Data/class_data.csv"
+    MANAGER_FILE_PATH = "../Data/manager_data.csv"
+    MEMBER_FILE_PATH = "../Data/member_data.csv"
+    SUBSCRIPTION_FILE_PATH = "../Data/subscription_data.csv"
 
     def class_by_id(self, id: int):
         with open(self.CLASS_FILE_PATH, mode='r') as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
                 if row['id'] == str(id):
-                    return row
+                    return FitnessClass(**row)
                 else:
                     continue
 
@@ -21,6 +25,6 @@ class Data:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
                 if row['id'] == str(id):
-                    return row
+                    return Member(**row)
                 else:
                     continue
