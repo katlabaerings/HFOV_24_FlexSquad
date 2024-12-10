a05fe3456efddf2cc5b36f0448ae9b1b808ba8a2
Tomas Palsson <tomas2212042710@gmail.com>
2024-12-10 11:44:27 +0000

    Virtual class

--- UI/Menus/virtual_menu.py
+++ UI/Menus/virtual_menu.py
@@ -1,41 +1,53 @@
 import npyscreen
-from UI.interfaces.i_menu import IMenu
-from UI.form_enums import Form
-from Logic.class_logic import ClassLogic
 
-"""This is the interface that lets users select what virtual class to attend"""
+from UI.interfaces.i_menu import IMenu
+from Data.read_data import ReadData
+from UI.form_enums import Form
 
 
 class VirtualMenu(IMenu):
     def create(self):
-        class_logic = ClassLogic()
-        all_classes = class_logic.get_virtual_classes()
-        self.class_names = [
-            f"{cls.class_name} ({cls.current_capacity}/{cls.max_capacity}) - {cls.time} on {cls.date}"
-            for cls in all_classes
-            if str(self.parentApp.user_id) in cls.members.split()
-        ]
+        self.add(npyscreen.FixedText, value="Classes", editable=False, color="STANDOUT")
 
-        self.class_selector = self.add(
+        self.class_menu = self.add(
             npyscreen.BoxTitle,
-            max_height=10,
-            name="Available Classes:",
-            values=self.class_names,
+            name="Classes",
+            values=[],
+            max_height=20,
+            rely=2,
             scroll_exit=True,
         )
+        self.class_menu.when_value_edited = self.on_select
 
-    # def fetch_virtual_classes_by_id(self, id):
-    #     ret_lis = []
+    def set_classes(self):
+        self.fitness_classes = ReadData().get_all_classes()
+        self.class_menu.values = [
+            f"{cls.class_name} ({cls.current_capacity}/{cls.max_capacity}) - {cls.time} on {cls.date}"
+            for cls in self.fitness_classes
+            if str(self.parentApp.user_id) in cls.members.split()
+            and cls.locality == "V"
+        ]
+        if not self.class_menu.values:
+            self.class_menu.values = ["No virtual classes found."]
 
-    #     npyscreen.notify_confirm(str(all_classes))
-    #     for a_class in all_classes:
-    #         if id in [int(b_class) for b_class in a_class.members.split()]:
-    #             ret_lis.append(a_class)
-    #     self.class_names = ret_lis
-    #     self.display()
+        self.class_menu.display()
+
+    def on_select(self):
+        if self.class_menu.value is not None and self.fitness_classes:
+            selected_class = self.fitness_classes[self.class_menu.value]
+            details = (
+                f"Class: {selected_class.class_name}\n"
+                f"Trainer: {selected_class.trainer_id.firstname} {selected_class.trainer_id.lastname}\n"
+                f"Capacity: {selected_class.current_capacity}/{selected_class.max_capacity}\n"
+                f"Time: {selected_class.time}\n"
+                f"Date: {selected_class.date}\n"
+                f"Locality: {selected_class.locality}\n"
+                f"Link: {selected_class.link}"
+            )
+            npyscreen.notify_confirm(details, title="Fitness Class Details")
 
     def on_ok(self):
         self.parentApp.switchForm(Form.MAIN)
 
     def on_cancel(self):
-        return super().on_cancel()
+        self.parentApp.setNextForm(None)
